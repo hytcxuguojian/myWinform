7abae229fe75f85ac5ab8bfd1fbccbf9fc17311d
zixuwuyou <1075737942@qq.com>
2014-08-25 20:01:40 +0800

    自定义协议的局域通讯Demo
    自定义协议的局域通讯Demo，使用UDP协议。

---
+++ chattingOnline/chattingOnline/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace chattingOnline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.iptextBox = new System.Windows.Forms.TextBox();
+            this.inputMessage = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textbox_nick = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // iptextBox
+            // 
+            this.iptextBox.Location = new System.Drawing.Point(45, 21);
+            this.iptextBox.Name = "iptextBox";
+            this.iptextBox.Size = new System.Drawing.Size(166, 21);
+            this.iptextBox.TabIndex = 1;
+            this.iptextBox.Text = "255.255.255.255";
+            // 
+            // inputMessage
+            // 
+            this.inputMessage.Location = new System.Drawing.Point(12, 342);
+            this.inputMessage.Multiline = true;
+            this.inputMessage.Name = "inputMessage";
+            this.inputMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputMessage.Size = new System.Drawing.Size(347, 51);
+            this.inputMessage.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(4, 48);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(355, 288);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(365, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(93, 292);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "当前在线：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "地址：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "昵称：";
+            // 
+            // textbox_nick
+            // 
+            this.textbox_nick.Location = new System.Drawing.Point(254, 20);
+            this.textbox_nick.Name = "textbox_nick";
+            this.textbox_nick.Size = new System.Drawing.Size(100, 21);
+            this.textbox_nick.TabIndex = 8;
+            this.textbox_nick.Text = "胥国健";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 405);
+            this.Controls.Add(this.textbox_nick);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.inputMessage);
+            this.Controls.Add(this.iptextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox iptextBox;
+        private System.Windows.Forms.TextBox inputMessage;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textbox_nick;
+    }
+}
+
---
+++ chattingOnline/chattingOnline/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace chattingOnline
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ip;//ip地址
+        IPEndPoint ipep;
+        string nick;//用户昵称
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = "PUBLIC|" + this.inputMessage.Text + "|" + nick + "|" + this.inputMessage.Text.Length;
+            SendMessage(msg, ipep);
+        }
+
+        private void listen()
+        {
+            UdpClient uc = new UdpClient(9527);
+            while(true)
+            {
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any,0);
+                byte[] b = uc.Receive(ref ipep);
+                string msg = Encoding.Default.GetString(b);
+                string[] message = msg.Split('|');
+                if (message[0] == "INROOM")
+                {
+                    this.richTextBox1.Text += message[1] + "上线了" + "\r\n";
+                    this.listBox1.Items.Add(message[1]);
+                }
+                if (message[0] == "PUBLIC")
+                {
+                    if (message.Length > 4)
+                    {
+                        int talkcharnum = Convert.ToInt16(message[message.Length - 1]);
+                        string talkContent = msg.Substring(7, talkcharnum);
+                        string talker = message[message.Length - 2];
+                        this.richTextBox1.Text += talker + "说：" + talkContent + "\r\n";
+                    }
+                    else
+                    {
+                        this.richTextBox1.Text += message[2] + "说："+ message[1] + "\r\n";
+                    }
+                }
+                if (message[0] == "OUTROOM")
+                {
+                    this.richTextBox1.Text += message[1] + "下线了" + "\r\n";
+                    this.listBox1.Items.Remove(message[1]);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1.CheckForIllegalCrossThreadCalls = false;
+            Thread t = new Thread(new ThreadStart(listen));
+            t.IsBackground = true;
+            t.Start();
+            nick = this.textbox_nick.Text;
+            string msg = "INROOM|" + nick;
+            ip = this.iptextBox.Text;
+            ipep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            SendMessage(msg, ipep);
+        }
+
+        private void SendMessage(string Message, IPEndPoint ipep)
+        {
+            Byte[] bt = Encoding.Default.GetBytes(Message);
+            UdpClient udp = new UdpClient();
+            udp.Send(bt, bt.Length, ipep);
+            this.inputMessage.Clear();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string msg = "OUTROOM|" + nick;
+            SendMessage(msg,ipep);
+        }
+    }
+}
